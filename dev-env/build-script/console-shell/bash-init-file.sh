d0c28590fdda28f37dd383926b2320d94e061307
Marcin Falkowski <rhobar3@gmail.com>
2025-02-09 18:33:40 +0100

    console: add PS1_CUSTOM variable

--- dev-env/build-script/console-shell/bash-init-file.sh
+++ dev-env/build-script/console-shell/bash-init-file.sh
@@ -3,3 +3,4 @@ source ~/.bashrc
 export PATH="{{.AddToPath}}:$PATH"
 {{end -}}
 PS1="[{{.PromptText}}] ${PS1}"
+export PS1_CUSTOM="{{.PromptText}}"   # to use with prompt engines (like starship.rs), that typically override PS1
