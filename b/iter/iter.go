56214e91afeb39a2f0f504fd3cd643366b92d62a
Marcin Falkowski <rhobar3@gmail.com>
2025-09-09 14:36:49 +0200

    iter Map() fix - get rid of alias, it doesn't work correctly

--- b/iter/iter.go
+++ b/iter/iter.go
@@ -2,8 +2,6 @@ package iter
 
 import "iter"
 
-type Seq[T any] = iter.Seq[T]
-
 func FindAny[T any](i iter.Seq[T], condition func(elem T) bool) *T {
 	for elem := range i {
 		if condition(elem) {
@@ -17,7 +15,7 @@ func AnyMatch[T any](i iter.Seq[T], condition func(elem T) bool) bool {
 	return FindAny(i, condition) != nil
 }
 
-func Map[T any, K any](i iter.Seq[T], mapper func(elem T) K) Seq[K] {
+func Map[T any, K any](i iter.Seq[T], mapper func(elem T) K) iter.Seq[K] {
 	return func(yield func(K) bool) {
 		for elem := range i {
 			if !yield(mapper(elem)) {
