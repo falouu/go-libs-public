50916567b9ec24c9dfeb0a592d1dc26116e8b1a1
Marcin Falkowski <rhobar3@gmail.com>
2026-01-11 20:40:46 +0100

    build-script: run with context

--- dev-env/build-script/api/api.go
+++ dev-env/build-script/api/api.go
@@ -29,6 +29,8 @@ type Template struct {
 }
 
 type RequirementEnvironment interface {
+	// gitignored dir for temp files
 	BuildDir() string
+	// set in Specification. Use console_shell.Run*(spec Specification) to fill it automatically
 	RootDir() string
 }
--- dev-env/build-script/build-script.go
+++ dev-env/build-script/build-script.go
@@ -16,12 +16,28 @@ type RequirementResult = api.RequirementResult
 type RequirementInfo = api.RequirementInfo
 
 func Run(spec *Specification) error {
+	return RunWithContext(func(ctx Context) (*Specification, error) {
+		return spec, nil
+	})
+}
 
+type SpecSupplier func(context Context) (*Specification, error)
+
+// Prefer over Run() if the context is needed to initialize Specification (e.g. access to script instance)
+func RunWithContext(spec SpecSupplier) error {
 	script, err := script.Init()
 	if err != nil {
 		return err
 	}
+	ctx := Context{Script: script}
+	s, err := spec(ctx)
+	if err != nil {
+		return err
+	}
+	return run(s, script)
+}
 
+func run(spec *Specification, script script.Script) error {
 	rootDir := script.Dir()
 	if spec.RootDir != "" {
 		if filepath.IsAbs(spec.RootDir) {
@@ -32,6 +48,10 @@ func Run(spec *Specification) error {
 	}
 	spec.RootDir = rootDir
 	return console_shell.Run(spec)
+}
+
+type Context struct {
+	Script script.Script
 }
 
 type FuncRequirement func(env RequirementEnvironment) (*RequirementResult, error)
